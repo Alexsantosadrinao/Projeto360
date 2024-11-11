cde6d7b69f44e834d4e8f5854b669638b09e4e1e
alex22 <alexmar22@outlook.com>
2024-11-11 12:17:01 -0300

    Deletar

--- src/paginas/Home/Usuarios/Usuarios.js
+++ src/paginas/Home/Usuarios/Usuarios.js
@@ -1,28 +1,52 @@
-import { Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { Sidebar } from "../../../Componentes/Sidebar/Sidebar";
 import { Topbar } from "../../../Componentes/Topbar/Topbar";
 import { Link } from 'react-router-dom';
 import style from "./Usuarios.module.css";
 import { MdDelete, MdEdit } from "react-icons/md";
-import { useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 import UsuarioAPI from "../../../Services/usuarioAPI";
 
 export function Usuario() {
     const [usuarios, setUsuarios] = useState([]);
+    const [mostarModal, setMostarModal] = useState(false);
+    const [usuarioSelecionado, setUsuarioSelecionado] = useState(null)
 
-    async function carregarUsuarios(){
-        try{
+
+    const handClikDeletar = (usuario) => {
+        setUsuarioSelecionado(usuario)
+        setUsuarioSelecionado(true);
+    };
+
+    const handleDeletar = async () => {
+        try {
+            await UsuarioAPI.deletarAsync(usuarioSelecionado.id);
+            setUsuarios(usuarios.filter(u => u.id !== usuarioSelecionado.id));
+        } catch (error) {
+            console.error("Erro ao deletar usuario:", error);
+        } finally {
+            handleFecharModal()
+        }
+    };
+
+    const handleFecharModal = () => {
+        setMostarModal(false);
+        setUsuarioSelecionado(null);;
+    }
+
+    async function carregarUsuarios() {
+        try {
             const listaUsuarios = await UsuarioAPI.listarAsync(true);
             setUsuarios(listaUsuarios);
-        }catch (error){
+        } catch (error) {
             console.error("Erro ao carregar usuario:", error);
         }
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         carregarUsuarios();
-    },[]);
-    
+    }, []);
+
     return (
         <Sidebar>
             <Topbar>
@@ -53,9 +77,10 @@ export function Usuario() {
                                             <Link to='/usuario/editar' state={Usuario.id} className={style.botao_editar}>
                                                 <MdEdit />
                                             </Link>
-                                            <Link to='/usuario/deletar' state={Usuario.id}>
-                                                <MdDelete />
-                                            </Link>
+                                            <button onClick={() => handClikDeletar(Usuario)} className={style.bota_deletar}>
+                                                <MdDelete/>
+                                            </button>
+
                                         </td>
 
                                     </tr>
@@ -65,7 +90,23 @@ export function Usuario() {
                         </Table>
 
                     </div>
+                    <Modal show={mostarModal} onHide={handleFecharModal}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Confirmar</Modal.Title>
+                        </Modal.Header>
 
+                        <Modal.Body>
+                            Tem certeza que deseja deletar o usuario {usuarioSelecionado?.nome}?
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleFecharModal}>
+                                Cancelar
+                            </Button>
+                            <Button variant="danger" onClick={handleDeletar}>
+                                Deletar
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </div>
             </Topbar>
         </Sidebar>
